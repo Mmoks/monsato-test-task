5897ec1a4edf426e0dcc244a1aa036db100eae36
Mmoks <maksim.andrux@gmail.com>
2019-06-09 19:26:13 +0300

    initial build

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PayeverTechTask
+# MonsatoTechTask
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to payever-tech-task!');
+    expect(page.getTitleText()).toEqual('Welcome to monsato-tech-task!');
   });
 
   afterEach(async () => {
